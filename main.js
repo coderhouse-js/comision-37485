2baa3db8340e02d650d09973924ddc26af521924
Toto <toto@gmail.com>
2022-07-07 17:32:14 -0300

    Clase de funciones

---
+++ main.js
@@ -0,0 +1,20 @@
+let nombreDelCurso = "toto";
+
+function agregarAlCarrito(producto, stock) {
+    const tenemosStock = validarStock(stock);
+    if(tenemosStock === 'Tenemos stock'){
+        console.log('Agregas el producto al carrito: ' + producto);
+    }else{
+        console.log('Lo siento, subio mucho el dolar :(');
+    }
+}
+
+function validarStock(stockDelProducto) {
+    if(stockDelProducto > 0){
+        return 'Tenemos stock';
+    }else{
+        return 'No tenemos stock';
+    }
+}
+
+agregarAlCarrito('Zapatillas de toto', 0) 
