ed7c5ebb6009715a4663c103c2228ada80c2fb7c
Toto Hourcade <hourcadecristian@gmail.com>
2022-07-13 18:41:48 -0300

    Clase de Arrays

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-let nombreDelCurso = "toto";
+const carrito = [];
 
-function agregarAlCarrito(producto, stock) {
-    const tenemosStock = validarStock(stock);
-    if(tenemosStock === 'Tenemos stock'){
-        console.log('Agregas el producto al carrito: ' + producto);
-    }else{
-        console.log('Lo siento, subio mucho el dolar :(');
-    }
+function agregarAlCarrito(producto){
+    carrito.push(producto);
+    console.log(carrito);
 }
 
-function validarStock(stockDelProducto) {
-    if(stockDelProducto > 0){
-        return 'Tenemos stock';
-    }else{
-        return 'No tenemos stock';
+// Deberia llamarlo el usuario al agregar un producto al carrito
+agregarAlCarrito({id: 9123, name: "Gorra adidas", price: 900})
+agregarAlCarrito({id: 3727, name: "Zapas adidas", price: 200})
+agregarAlCarrito({id: 8818, name: "Remera adidas", price: 100})
+
+
+function borrarProductoDelCarrito(idDelProducto){
+    const index = carrito.findIndex((producto) => producto.id === idDelProducto);
+
+    if(index !== -1) {
+        carrito.splice(index, 1);
     }
+
+    console.log(carrito);
 }
 
-agregarAlCarrito('Zapatillas de toto', 0);
+// Deberia llamarlo el usuario al hacer click ene l boton de borrar
+borrarProductoDelCarrito(9123);
+borrarProductoDelCarrito(8818);
+borrarProductoDelCarrito(3);
